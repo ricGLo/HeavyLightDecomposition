a8a673f2bc3f272294745778cc8553a86d11386a
ricGLo <ricalgp00@gmail.com>
2023-08-13 15:38:32 -0600

    translate commentaries

--- src/HLD.cpp
+++ src/HLD.cpp
@@ -2,21 +2,38 @@
 using namespace std;
 #define MAX 100000
 
-long long ST[4*MAX]; //Segment tree
-vector<vector<int>> AdjList; //Lista de adyacencia del arbol.
-vector<long long> values; //Alamacena el valor (peso) de cada nodo.
-vector<int> parent; //Almacena el padre de cada nodo.
-vector<int> heaviest; //Almacena el hijo mas pesado de cada nodo, es decir el hijo que tiene mas descendientes.
-vector<int> depth; //Almacena la profundidad de cada nodo.  
-vector<int> HLD; //Se guardan los nodos separados por caminos. Si el primer camino consta de k nodos, los primeros k nodos del vector contienen dichos k nodos empezando por la raiz del camino hasta el nodo hoja del camino. Luego se guarda otro camino de la mismo manera y asi sucesivamente.
-vector<int> map_to_HLD; //En la posicion j del vector se guarda el indice donde se encuentra el nodo j en el vector HLD.
-vector<int> roots; //Almacena la raiz del path en el que se encuentra cada nodo.
+//Segment tree
+long long ST[4*MAX]; 
+
+//Adjacent List of the tree
+vector<vector<int>> AdjList; 
+
+//Stores the value (weight) of the node
+vector<long long> values; 
+
+//Stores the parent Id of each node
+vector<int> parent; 
+
+//Stores the heaviest child of the node. This is, the child with mode descendants.
+vector<int> heaviest; 
+
+// Stores the depth of each node in the tree  
+vector<int> depth; 
+
+// It stores nodes Id's separated by paths. For instance, if the first path consists on k nodes, the first k places in the vector contain those k nodes Id's in the order corresponding to the path beginning with the root of the path to the leaf node of the path. Then the vector stores another path in the same way and so on.
+vector<int> HLD; 
+
+//The j-th entry of this vector stores the index where node j is in the HLD vector.
+vector<int> map_to_HLD; 
+
+// Stores the root of the path to which the node belongs. Recall that each of the nodes only belongs to one path.
+vector<int> roots; 
 
 /////////////////////////////////////////  SEGMENT TREE IMPLEMENTATION  //////////////////////////////////////
 /* 
-[i_l, i_r] : Es el intervalo del indice actual
-q_idx : indice a actualizar
-val : valor nuevo de q_idx  */
+[i_l, i_r] : Currently index interval
+q_idx : index to update
+val : new value of q_idx  */
 
 void updateUtil(int index, int i_l, int i_r, int q_idx, long long val){
     if (i_l == i_r){
@@ -33,9 +50,9 @@ void updateUtil(int index, int i_l, int i_r, int q_idx, long long val){
     ST[index] = ST[index*2+1] + ST[index*2+2];
 }
 
-// Function to update a range of values in segment tree
+// Function to update a value in the segment tree
 void update(int n, int q_idx, int val){
-   updateUtil(0, 0, n-1, q_idx, (long long) val); //Se hace cast de int a long long
+   updateUtil(0, 0, n-1, q_idx, (long long) val); // cast val to long long
 }
 
 long long queryST_util(int index, int i_l, int i_r, int q_l, int q_r){
@@ -81,14 +98,14 @@ void constructST(int n){
 
 // Depth First Search
 int DFS(int v, int dad, int d){
-    int size = 1; //tamano del subarbol con raiz v
-    int max_size = -INT_MAX; //tamanio del subarbol mas grande que tiene como raiz algun nodo hijo de v, se inicializa en -infty
-    depth[v] = d; //profundidad
-    parent[v] = dad; //Asignar padre de v
+    int size = 1; // size of the subtree with root v
+    int max_size = -INT_MAX; //size of bigger subtree whose root is some child of v, it is initialized at -infty
+    depth[v] = d; 
+    parent[v] = dad; //assign parent of v
     for(int i = 0; i < AdjList[v].size(); i++){
         int u = AdjList[v][i];
         int subsize = DFS(u, v, d+1);
-        if(subsize > max_size){ //actualizar quien es el subarbol mas pesado
+        if(subsize > max_size){ //update which is the heaviest subtree
             heaviest[v] = u;
             max_size = subsize;
         }
@@ -98,17 +115,17 @@ int DFS(int v, int dad, int d){
 }
 
 void make_HLD(int N){
-    int current_idx = 0; //indice actual en el vector HLD
+    int current_idx = 0; // currently index in the vector HLD
     for(int i = 0; i < N; i++){
-        //Si i es el nodo raiz o si i no es el subarbol mas grande entre los de sus hermanos, entonces i es raiz de un path
+        // If i is the root of the entire tree, or if i is not the heaviest subtree among its siblings, then i is the root of a path
         if(parent[i] == -1 || heaviest[parent[i]] != i){
-            int j = i; //nodo en el recorrido del path
-            while(j != -1){ //recorrer el path hasta llegar a un nodo hoja
+            int j = i; //node in the travel of the path
+            while(j != -1){ // travel the path until reaching a leaf node
                 HLD[current_idx] = j;
                 roots[j] = i;
                 map_to_HLD[j] = current_idx;
                 current_idx++;
-                j = heaviest[j]; //el siguiente nodo en el path es el mas pesado de los hijos de j
+                j = heaviest[j]; //next node in the path is the heaviest child of j
             }       
         }
     }
@@ -116,19 +133,19 @@ void make_HLD(int N){
 
 long long sumPath(int a, int b, int n){
     long long sum = 0;
-    while(roots[a] != roots[b]){ //Mientras que los paths de a y b sean distintos
-        if(depth[roots[a]] > depth[roots[b]]) // Deseamos que b sea el nodo cuyo path tenga la raiz mas profunda
+    while(roots[a] != roots[b]){ //While a and b are in distinct paths
+        if(depth[roots[a]] > depth[roots[b]]) // b is needed to be deeper than a
             swap(a, b);
         
         sum += queryST(n, map_to_HLD[roots[b]], map_to_HLD[b]);
         b = parent[roots[b]];
     }
-    //Ahora a y b estan en el mismo path
+    //Now a and b are in the same path
 
-    if(depth[a] > depth[b])//Deseamos que b sea el nodo con mayor profundidad
+    if(depth[a] > depth[b]) // b is needed to be deeper than a
         swap(a, b); 
 
-    sum += queryST(n, map_to_HLD[a], map_to_HLD[b]); //Sumar el trozo deseado del camino 
+    sum += queryST(n, map_to_HLD[a], map_to_HLD[b]); // Add up desire part od the path
     return sum;
 }
 
@@ -137,13 +154,13 @@ int main(){
     cin.tie(0);
     cout.tie(0); 
 
-    //Variables para leer INPUT
+    //variables to read the INPUT
     int N, Q, dad, a, b;
     long long num;
     char op;
     cin >> N >> Q;
 
-    //Inicializar vectores
+    // Initialize vectors
     AdjList.assign(N, vector<int>());
     values.assign(N, 0);
     parent.assign(N, -1);
@@ -153,22 +170,22 @@ int main(){
     roots.assign(N, 0);
     map_to_HLD.assign(N, 0);
 
-    //Obtener los valores de cada nodo
+    // Get values of each node
     for(int i = 0; i < N; i++)
         cin >> values[i];
 
-    //Crear la lista de adyacencia del arbol
+    // Create adjacency list of the tree
     for(int i = 1; i < N; i++){
         cin >> dad;
         AdjList[dad].push_back(i);
     }
 
-    //Construir las estructuras de datos
-    DFS(0, -1, 0); //Recorrido del arbol en profundiad
-    make_HLD(N);   //Realizar el Heavy Light Decomposition
-    constructST(N); //Construir Segment Tree
+    // Construct Data Structures
+    DFS(0, -1, 0); // Travel the tree via DFS
+    make_HLD(N);   // Make Heavy Light Decomposition
+    constructST(N); // Construct Segment Tree
 
-    //Peticiones
+    //Queries
     for(int i = 0; i < Q; i++){
         cin >> op >> a >> b;
         if(op == 'U') //Update
